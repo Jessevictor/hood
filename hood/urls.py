e2041802a59af9101fb752421b5581c2808403f9
jessevictor <kongoijesse97@gmail.com>
2020-11-03 06:12:11 +0300

    change hood urls and settings

--- hood/urls.py
+++ hood/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -16,3 +18,5 @@ urlpatterns = [
     path('postImage/', views.create_post, name = 'post_image'),
     path('search/', views.search_business, name='search'),
 ]
+if settings.DEBUG:
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
